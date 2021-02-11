886d2154821070774e4df5b10741da4b9f7f110f
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2021-02-11 19:01:07 +0530

    added files

---
+++ app.js
@@ -0,0 +1,33 @@
+//selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event listenors
+todoButton.addEventListener('click' , addTodo);
+
+//Funtions
+function addTodo(e){
+    e.preventDefault();
+    console.log('Button-clicked');
+    // creating the div 
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // creating the li 
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Adding Buttons for completed
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i classs="fas fa-check"><i/>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    // Adding Buttons for trash
+    const completedButton = document.createElement('button');
+    trashButton.innerHTML = '<i classs="fas fa-trash"><i/>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+}
