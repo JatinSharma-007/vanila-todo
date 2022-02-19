5bd341f979d688cb3be687eabdcd9bad9c689ee9
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-02-19 12:14:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
-// Event listenors
+
 todoButton.addEventListener('click' , addTodo);
 
 //Funtions
